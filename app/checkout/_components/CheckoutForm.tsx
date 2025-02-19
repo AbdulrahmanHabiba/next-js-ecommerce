05c8417ab110b757219f8cd58db65370d955f34c
Abdulrahman Habiba <77078433+AbdulrahmanHabiba@users.noreply.github.com>
2025-02-20 01:44:14 +0200

    Update return url in CheckoutForm.tsx

--- app/checkout/_components/CheckoutForm.tsx
+++ app/checkout/_components/CheckoutForm.tsx
@@ -66,7 +66,7 @@ const handleSubmit = async (event: React.FormEvent) => {
     const {paymentIntent, error} = await stripe.confirmPayment({
         elements,
         clientSecret,
-        confirmParams: {return_url: "http://localhost:3000/success"},
+        confirmParams: {return_url: "https://next-js-ecommerce-liard.vercel.app/success"},
     });
 
     if (error) {
